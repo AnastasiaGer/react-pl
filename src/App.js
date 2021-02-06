0ce7c9d7668ef1c3bf5a61e94d70c84b651648df
AnastasiaGer <aagermanova@gmail.com>
2021-02-06 13:49:13 -0800

    button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
 import './App.css';
+import React, {Component} from 'react';
+import Car from './Car/Car'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+
+export default class App extends Component {
+  state = {
+    cars: [
+      {name: 'Ford', year: 2000},
+      {name: 'Mazda', year: 2001},
+      {name: 'Audi', year: 2002},
+    ],
+    pageTitle: 'React components',
+    showCars: false
+  }
+
+changeTitleHandler = (newTitle) => {
+  this.setState({
+    pageTitle: newTitle
+  })
 }
 
-export default App;
+toggleCarsHandler =() => {
+  this.setState({
+    showCars: !this.state.showCars
+  })
+}
+
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.pageTitle}</h1>
+
+        <button onClick={this.toggleCarsHandler}>Toggle cars</button>
+
+        { this.state.showCars ?
+        this.state.cars.map((car, index) => {
+            return (
+              <Car 
+              key={index}
+              name={car.name}
+              year={car.year}
+              onChangeTitle={() => this.changeTitleHandler(car.name)}
+              />
+            )
+        })
+      :null}
+      </div>
+    );
+  }
+}
+
---
+++ src/Car/Car.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default (props) => (
+  <div>
+    <h2>Car name:  {props.name}</h2>
+    <p>Car year: {props.year}</p>
+    <button onClick = {props.onChangeTitle}>Click</button>
+  </div>
+  )
+
+
